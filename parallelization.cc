d3a82b76ff9ff85810c6d74c4b4f3408f074356b
gM <gabrielmorck@outlook.com>
2020-04-17 22:39:43 +0200

    Create parallelization.cc

---
+++ parallelization.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <omp.h>
+
+#define n 1000
+
+// Parallel function
+void Parallelization(unsigned th)
+{
+    unsigned i;
+    float a[th], b[th], c[th];
+    #pragma omp parallel for shared(a, b, c) private(i)
+    for(i=0;i<th;++i)
+    {
+        c[i] = a[i] + b[i];
+    }
+}
+
+int main()
+{
+    cout<<"OpenMP Parallelization\n";
+    // Parallelization
+    Parallelization(n);
+
+    return 0;
+}
